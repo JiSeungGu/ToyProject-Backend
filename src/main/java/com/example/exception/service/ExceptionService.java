9cdac6628db23dc72ad8a0f9745857328879d9a9
JiSeungGu <tmdrn457@naver.com>
2023-09-18 10:15:47 +0900

    Logback Console, File ,+ ERROR LOG

--- src/main/java/com/example/exception/service/ExceptionService.java
+++ src/main/java/com/example/exception/service/ExceptionService.java
@@ -4,6 +4,8 @@ import com.example.exception.ErrorCode;
 import com.example.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,12 +19,14 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class ExceptionService {
+  Logger logger = LoggerFactory.getLogger(this.getClass());
 
   public void throwException() {
     throw new RuntimeException("RuntimeException");
   }
   public String test() {
     if(true) {
+      logger.error("FAILED_TO_VALIDATE_APPLE_LOGIN");
       throw new CustomException(ErrorCode.FAILED_TO_VALIDATE_APPLE_LOGIN);
     }
     return "test";
